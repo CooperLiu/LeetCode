d194f2034b11eb18ad395fd81eded3b23f3a911a
Cooperliu <Cooperliu@DESKTOP-DFCUGBH>
2020-02-01 11:19:24 +0800

    #121

---
+++ LeetCodeSolutionTests/Solution121_Tests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeSolution;
+using Xunit;
+
+namespace LeetCodeSolutionTests
+{
+    public class Solution121_Tests
+    {
+        [Fact]
+        public void MaxProfit_Test()
+        {
+            var solution = new Solution121();
+
+            var res = solution.MaxProfit(new[] {7, 1, 5, 3, 6, 4});
+
+            Assert.Equal(5,res);
+        }
+    }
+}
