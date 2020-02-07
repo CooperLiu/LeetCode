3e2f8ea182743b648bf7729aabb2d6bfa51a9b8e
Cooperliu <Cooperliu@DESKTOP-DFCUGBH>
2020-02-07 22:30:44 +0800

    #641 #240

--- LeetCodeSolutionTests/Solution121_Tests.cs
+++ LeetCodeSolutionTests/Solution121_Tests.cs
@@ -40,7 +40,29 @@ namespace LeetCodeSolutionTests
 
             var result = solution.Search(new[] { -1, 0, 3, 5, 9, 12 }, 9);
 
-            Assert.Equal(4,result);
+            Assert.Equal(4, result);
+        }
+    }
+
+    public class Solution240_Tests
+    {
+        [Fact]
+        public void SearchMatrix_Test()
+        {
+            var matrix = new int[,]
+            {
+                { 1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                { 18, 21, 23, 26, 30}
+            };
+
+            var solution = new Solution240();
+
+            var res = solution.SearchMatrix(matrix, 9);
+
+            Assert.True(res);
         }
     }
 }
